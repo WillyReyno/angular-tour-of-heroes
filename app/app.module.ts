bad7dbab55759e2cba0dc32b79b2ba32e6f0dd69
willy <reyno.willy@gmail.com>
2017-01-12 15:45:54 +0100

    Created services

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,9 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import {AppComponent}  from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -14,8 +14,7 @@ import {HeroDetailComponent} from './hero-detail.component';
     AppComponent,
     HeroDetailComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 
-export class AppModule {
-}
+export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,23 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from  '@angular/core';
 
-import {Hero} from './hero';
+import { Hero } from './hero';
 
 @Component({
-  selector: 'my-hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{ hero.name }} details!</h2>
-    <div><label>id : </label>{{ hero.id }}</div>
-    <div>
-      <label>name : </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  </div>`
+    selector: 'my-hero-detail',
+    templateUrl: './hero-detail.component.html',
 })
 
-
-
 export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+    @Input()
+    hero: Hero;
 }
