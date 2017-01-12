21b42e7cccd814fa7435874e25ff2ce6b22eeae0
willy <reyno.willy@gmail.com>
2017-01-12 15:46:25 +0100

    useless comment removed

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
-//import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
